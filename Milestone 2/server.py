bcc2a27f301b852369ad9586fd7744badf9caecc
Carl Neumann <carlneumann@Mac-mini-von-Carl.fritz.box>
2023-05-16 18:25:07 +0200

    updated

--- Milestone 2/server.py
+++ Milestone 2/server.py
@@ -41,32 +41,62 @@ class KeyValueServer:
 
     def handle_client(self, client_socket):
         while True:
-            data = client_socket.recv(1024).decode()
+            data = client_socket.recv(1024)
             if not data:
                 break
 
-            command, *args = data.strip().split('|')
+            command, *args = data.decode().split('|')
 
             if command == 'put':
                 key, value = args
-                if key in self.storage:
-                    response = f"put_update {key}"
-                else:
-                    response = f"put_success {key}"
-                self.storage[key] = value
+                with self.cache_lock:
+                    if self.cache_strategy == self.FIFO:
+                        if len(self.fifo_cache) >= self.cache_size:
+                            self.fifo_cache.popitem(last=False)
+                        self.fifo_cache[key] = value
+                    elif self.cache_strategy == self.LRU:
+                        put_lru_value(key, value)
+                    elif self.cache_strategy == self.LFU:
+                        if key in self.lfu_cache:
+                            self.lfu_cache[key] = (self.lfu_cache[key][0] + 1, value)
+                        else:
+                            if len(self.lfu_cache) >= self.cache_size:
+                                min_key = min(self.lfu_cache, key=lambda k: self.lfu_cache[k][0])
+                                del self.lfu_cache[min_key]
+                            self.lfu_cache[key] = (1, value)
+                    with shelve.open('storage.db') as db:
+                        if key in self.storage:
+                            response = f"put_update {key}"
+                        else:
+                            response = f"put_success {key}"
+                        db[key] = value
             elif command == 'get':
                 key = args[0]
-                if key in self.storage:
-                    response = f"get_success {key}"
+                with self.cache_lock:
+                    if self.cache_strategy == self.FIFO:
+                        result = self.fifo_cache.get(key)
+                    elif self.cache_strategy == self.LRU:
+                        result = get_lru_value(key)
+                    elif self.cache_strategy == self.LFU:
+                        result = self.lfu_cache.get(key, (None, None))[1]
+
+                if result is None:
+                    with shelve.open('storage.db') as db:
+                        if key in db:
+                            result = db.get(key)
+                            response = f"get_success {key} {result}"
+                        else:
+                            response = f"get_error {key}"
                 else:
-                    response = f"get_error {key}"
+                    logging.info(f'Cache hit for key: {key}')
             elif command == 'delete':
                 key = args[0]
-                if key in self.storage:
-                    value = self.storage.pop(key)
-                    response = f"delete_success {key} {value}"
-                else:
-                    response = f"delete_error {key}"
+                with shelve.open('storage.db') as db:
+                    if key in db:
+                        result = db.pop(key)
+                        response = f"delete_success {key} {result}"
+                    else:
+                        response = f"delete_error {key}"
             else:
                 response = f"error Invalid command: {command}"
 
@@ -86,7 +116,6 @@ class KeyValueServer:
                 client_thread.start()
 
 
-
 @lru_cache
 def get_lru_value(key):
     return None
@@ -95,72 +124,6 @@ def get_lru_value(key):
 def put_lru_value(key, value):
     get_lru_value.cache_clear()
     get_lru_value(key)
-
-
-def handle_client(client_socket):
-    with client_socket:
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-
-            command, *args = data.decode().split('|')
-
-            if command == 'put':
-                key, value = args
-                with cache_lock:
-                    if cache_strategy == FIFO:
-                        if len(fifo_cache) >= cache_size:
-                            fifo_cache.popitem(last=False)
-                        fifo_cache[key] = value
-                    elif cache_strategy == LRU:
-                        put_lru_value(key, value)
-                    elif cache_strategy == LFU:
-                        if key in lfu_cache:
-                            lfu_cache[key] = (lfu_cache[key][0] + 1, value)
-                        else:
-                            if len(lfu_cache) >= cache_size:
-                                min_key = min(lfu_cache, key=lambda k: lfu_cache[k][0])
-                                del lfu_cache[min_key]
-                            lfu_cache[key] = (1, value)
-
-                with shelve.open('storage.db') as db:
-                    db[key] = value
-                client_socket.sendall(b'OK')
-            elif command == 'get':
-                key = args[0]
-                with cache_lock:
-                    if cache_strategy == FIFO:
-                        result = fifo_cache.get(key)
-                    elif cache_strategy == LRU:
-                        result = get_lru_value(key)
-                    elif cache_strategy == LFU:
-                        result = lfu_cache.get(key, (None, None))[1]
-
-            if result is None:
-                with shelve.open('storage.db') as db:
-                    result = db.get(key, 'Key not found')
-            else:
-                logging.info(f'Cache hit for key: {key}')
-            client_socket.sendall(result.encode())
-
-
-def main():
-    host = '127.0.0.1'
-    port = 65431
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((host, port))
-        server_socket.listen()
-
-        while True:
-            client_socket, addr = server_socket.accept()
-            logging.info(f'Connected by {addr}')
-            threading.Thread(target=handle_client, args=(client_socket,)).start()
-
-
-if __name__ == '__main__':
-    main()
 
 
 def main():
@@ -173,9 +136,9 @@ def main():
     parser.add_argument("-ll", "--log-level", default="INFO", help="Log level")
 
     args = parser.parse_args()
-
     server = KeyValueServer(args.address, args.port, args.directory, args.cache_size, args.log_file, args.log_level)
     server.start()
+
 
 if __name__ == "__main__":
     main()
